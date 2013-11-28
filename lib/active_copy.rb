347e5193f6e5bd541f88a45de141baa47d9823f1
Tom Scott <tscott@telvue.com>
2013-11-28 03:58:20 -0500

    Add version to lib/active_copy.rb

--- lib/active_copy.rb
+++ lib/active_copy.rb
@@ -1,12 +1,10 @@
-require 'bundler'
-Bundler.require :active_copy
-
 require 'active_copy/attributes'
 require 'active_copy/base'
 require 'active_copy/markdown'
 require 'active_copy/template'
 require 'active_copy/utils'
 require 'active_copy/view_helper'
+require 'active_copy/version'
 
 # ActiveCopy reads Markdown files in +app/documents+ instead of a
 # database for your Rails models. Inspired by Jekyll, it uses compatible
