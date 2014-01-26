8fae18c4eee9e2af7f46b8ad30b0cb79bfa1aec2
Tom Scott <tscott@telvue.com>
2014-01-26 18:12:59 -0500

    Fix code blocks in renderer

--- lib/active_copy/renderer.rb
+++ lib/active_copy/renderer.rb
@@ -1,13 +1,16 @@
-require 'pygments'
 require 'redcarpet'
 
 module ActiveCopy
   class Renderer < Redcarpet::Render::HTML
     def block_code(raw_code, language)
-      code = "\n#{raw_code.strip}"
-      #Rails.logger.info "Rendered highlighted #{language} code block"
-      #Pygments.highlight code, lexer: language
-      "<pre>#{code}\n</pre>"
+      code = "#{raw_code}".strip
+
+      if defined? Pygments
+        Rails.logger.info "Rendered highlighted #{language} code block"
+        Pygments.highlight code, lexer: language
+      else
+        "<pre>#{code}</pre>"
+      end
     end
   end
 end
