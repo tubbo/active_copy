4f52a9c397035acc77821c6e717f74922332588e
Tom Scott <tscott@telvue.com>
2014-01-26 00:56:26 -0500

    Add ActiveCopy helper modules and ActiveCopy::Base.

---
+++ lib/active_copy/paths.rb
@@ -0,0 +1,48 @@
+module ActiveCopy
+  module Paths
+    # Return absolute path to public HTML file.
+    def index_path
+      "#{path}/index.html"
+    end
+
+    # Return absolute path to public cached copy.
+    def path
+      @public_path ||= if Rails.env.test?
+                         "#{Rails.root}/tmp/site/#{relative_path}"
+                       else
+                         "#{Rails.root}/public/#{relative_path}"
+                       end
+    end
+
+    # Return the collection_path in the instance.
+    def collection_path
+      self.class.collection_path
+    end
+
+    # Return relative path with the Rails.root/public part out.
+    def relative_path
+      @rel_path ||= begin 
+        date_array = id.split("-")[0..2]
+        date_path = date_array.join("/")
+        article_id = begin
+          str = id.gsub date_array.join("-"), ''
+          if str[0] == "-"
+            str[1..-1] 
+          else
+            str
+          end
+        end
+        "#{category}/#{date_path}/#{article_id}"
+      end
+    end
+
+    # Return absolute path to Markdown file on this machine.
+    def source_path options={}
+      @source_path ||= if options[:relative]
+        File.join collection_path, "#{id}.md"
+      else
+        File.join Rails.root, collection_path, "#{id}.md"
+      end
+    end
+  end
+end
